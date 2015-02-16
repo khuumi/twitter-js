178f67a55d0d04aae37f69c50b104c6cd0a6caa2
Daniel Maxson <dsm2157@columbia.edu>
2015-02-16 14:42:45 -0500

    added more styling/templates as well as a in memory data store

--- app.js
+++ app.js
@@ -4,27 +4,24 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 var swig = require('swig');
-
+var routes = require('./routes/');
 var PORT = 3000;
 
 
 var people = [{ name:'Full'}, {name: 'Stacker'},
               {name: 'Sonny'}];
 
+app.use(express.static(__dirname + '/public'));
 
-
+app.use('/', routes);
 app.use(morgan('dev'));
+
 app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 swig.setDefaults({ cache: false});
-
-app.get('/', function (req, res) {
-  res.render('index', {title: 'Hall of Fame',
-                       people: people});
-});
 
 
 
@@ -34,8 +31,6 @@ var server = app.listen(PORT, function() {
   var port = server.address().port;
 
   console.log('Twitter App listening on http://%s:%s', host, port);
-
-
 
 });
 
