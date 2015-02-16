26f9dc23b96f8236c08aac6ad23e3f2bedc709eb
Daniel Maxson <dsm2157@columbia.edu>
2015-02-16 14:14:04 -0500

    adding basic templating

--- app.js
+++ app.js
@@ -3,14 +3,31 @@
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var swig = require('swig');
 
 var PORT = 3000;
 
+
+var people = [{ name:'Full'}, {name: 'Stacker'},
+              {name: 'Sonny'}];
+
+
+
 app.use(morgan('dev'));
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false});
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('index', {title: 'Hall of Fame',
+                       people: people});
 });
+
+
+
 
 var server = app.listen(PORT, function() {
   var host = server.address().address;
